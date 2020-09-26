f353ec3674262970a8dc41ace54aa572a2997bbe
Lionel Sanchez <58988565+snLionel90@users.noreply.github.com>
2020-09-26 20:08:50 +0200

    Added IntaDonwloader for Linux
    F

---
+++ instaDL/instadownloader_linux.py
@@ -0,0 +1,18 @@
+
+import instaloader
+import sys
+
+print("sn.lionel90")
+
+def picture_download(username):
+    parser = instaloader.Instaloader()
+    """Creates a new folder for insta downloads"""
+    with settings(warn_only=True):
+        run('mkdir /home/snlionel90/InstaDonloads')
+        return parser.download_profile(username,profile_pic= True)
+
+if __name__ =="__main__":
+    #win_crack.mainloop()
+    user = input ("Enter Instagram Account: ")
+    picture_download(user)
+        
